ca22426c274dd27572c603b90051d8457ccbbf68
alexandrefcesar <31703202+alexandrefcesar@users.noreply.github.com>
2018-03-07 20:28:52 -0300

    Add files via upload

---
+++ Insertion.java
@@ -0,0 +1,41 @@
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Insertion {
+
+	public static int[] gerarVector(int n) {
+		int[] c = new int[n];
+		Random gerador = new Random();
+		for (int i = 0; i < n; i++) {
+			c[i] = gerador.nextInt(100);
+		}
+		return c;
+
+	}
+
+	private static void insertionSort(int[] v) {
+		
+		for (int i=0 ; i<v.length ;i++) {
+			int aux = v[i];
+			for(int j=i-1; j>=0 && v[j]>aux; j--) {
+				v[j+1]= v[j];
+				v[j]=aux;
+			}
+		}
+
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] v = gerarVector(20);
+		System.out.println("Vetor desordenado:");
+		System.out.println(Arrays.toString(v));
+		insertionSort(v);
+		System.out.println("Vetor Ordenado:");
+		System.out.println(Arrays.toString(v));
+
+	}
+
+}
